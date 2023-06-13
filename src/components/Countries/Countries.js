662dae32e166027a8a794564fb23c33a60aaff65
mamun-prodhan <mamunpr9@gmail.com>
2023-06-13 14:14:15 +0600

    props destructring

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import Country from '../Country/Country';
+import React, { useEffect, useState } from "react";
+import Country from "../Country/Country";
+import './Countries.css';
 
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
-    return (
-        <div>
-            <h1>React Rest Countries</h1>
-            {
-                countries.map(country => <Country
-                name={country.name.common}
-                population={country.population}
-                area={country.area}
-                ></Country>)
-            }
-        </div>
-    );
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <div>
+      <h1>React Rest Countries</h1>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Country.css';
 
-const Country = ({name, population, area}) => {
+const Country = (props) => {
+
+    const {name, population, area, flags} = props.country;
 
     return (
         <div className='countryCard'>
-            <h2>Country Name: {name}</h2>
+            <h2>Country Name: {name.common}</h2>
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
